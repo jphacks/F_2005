07d29185c1e0e9a67470ac1cce9fdc02e8711c57
kyanachana <tomomin001104@gmail.com>
2020-10-31 17:52:41 +0900

    clock実装

---
+++ my_app/lib/clock.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'dart:async';
+
+class Clock extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _ClockState();
+  }
+}
+
+class _ClockState extends State<Clock> {
+  String _time = '';
+
+  @override
+  void initState() {
+    Timer.periodic(
+      Duration(seconds: 1),
+      _onTimer,
+    );
+    super.initState();
+  }
+
+  void _onTimer(Timer timer) {
+    var now = DateTime.now();
+    var formatter = DateFormat('HH:mm:ss');
+    var formattedTime = formatter.format(now);
+    setState(() => _time = formattedTime);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      _time,
+      style: TextStyle(
+        fontSize: 60.0,
+        fontFamily: 'IBMPlexMono',
+      ),
+    );
+  }
+}
